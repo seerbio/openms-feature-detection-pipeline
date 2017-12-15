d7a55270cc375ebd6e98bf2c694cef11371defff
ZenBrayn <rwbenz@gmail.com>
2017-12-15 09:27:50 -0800

    Path bug fixes.

--- SCRIPTS/99_start_openms_container.sh
+++ SCRIPTS/99_start_openms_container.sh
@@ -8,7 +8,7 @@ docker_img_name=hroest/openms-executables-2.2
 input_data_dir=$1
 
 # The directory to write the results to
-output_dir=$2
+output_dir_name=$2
 
 # The path to the script to execute
 exe_script=$3
@@ -16,7 +16,7 @@ exe_script=$3
 # The directory path on the local system
 # where the pipeline processing will occur
 # and output files will be written
-output_data_dir=`pwd`/pipeline_processing
+output_data_dir=`pwd`/output_dir_name
 scripts_dir=`pwd`/SCRIPTS
 ini_dir=`pwd`/INI
 
@@ -24,9 +24,9 @@ ini_dir=`pwd`/INI
 # Start up the docker image
 docker run -it \
   -v $input_data_dir:/input_data \
-  -v $output_data_dir:/pipeline_processing \
+  -v $output_data_dir:/$output_dir_name \
   -v $scripts_dir:/SCRIPTS \
   -v $ini_dir:/INI \
-  -w $output_dir \
+  -w /$output_dir_name \
    $docker_img_name \
    $exe_script
