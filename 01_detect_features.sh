a83421d23f255370ca1c5de4a53a2f235743ba98
ZenBrayn <rwbenz@gmail.com>
2017-12-14 22:56:08 -0800

    Moved 99_ script to the SCRIPTS directory.

--- 01_detect_features.sh
+++ 01_detect_features.sh
@@ -2,7 +2,7 @@
 input_data_dir=$1
 
 # Pipeline, Step 1
-./99_start_openms_container.sh $input_data_dir ../SCRIPTS/01_run_pipeline_step01.sh
+./SCRIPTS/99_start_openms_container.sh $input_data_dir ../SCRIPTS/01_run_pipeline_step01.sh
 
 # Pipeline, Step 2
 cd pipeline_processing
@@ -10,7 +10,7 @@ cd pipeline_processing
 cd ..
 
 # Pipeline, Step 3
-./99_start_openms_container.sh $input_data_dir ../SCRIPTS/03_run_pipeline_step03.sh
+./SCRIPTS/99_start_openms_container.sh $input_data_dir ../SCRIPTS/03_run_pipeline_step03.sh
 
 # Pipeline, Step 4
 cd pipeline_processing
